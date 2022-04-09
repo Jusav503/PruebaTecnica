a06cf5a6e406f66aac1758ad6bd14e770f2fc29f
Justice Velasco <justicevgomez001@gmail.com>
2022-04-09 18:59:18 +0200

    ✨ CREATED: CRUD API

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\Subcategory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use Illuminate\Support\Str;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->sentence(2);
+        $subcategory = Subcategory::all()->random();
+
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => $this->faker->text(),
+            'price' => $this->faker->randomElement([19.99, 49.99, 99.99]),
+            'subcategory_name' => $subcategory->name,
+        ];
+    }
+}
